1566e877e0a10ff9e231a0db1a2097d014fa1120
Kai <kai.niklas@web.de>
2024-02-01 23:48:53 +0100

    init

--- src/Tables/Columns/InlineGauge.php
+++ src/Tables/Columns/InlineGauge.php
@@ -10,14 +10,8 @@ class InlineGauge extends Column
 {
     protected string $view = 'kainiklas-filament-inline-gauge::tables.columns.inline-gauge';
 
-    /**
-     * @var string | Closure | null
-     */
     protected string | Closure | null $sectionColor = null;
 
-    /**
-     * @var string | Closure | null
-     */
     protected string | Closure | null $shapeColor = null;
 
     /**
@@ -35,10 +29,6 @@ class InlineGauge extends Column
      */
     protected int | Closure | null $upperBound = null;
 
-
-    /**
-     * @param  string | Closure | null  $color
-     */
     public function shapeColor(string | Closure | null $color): static
     {
         $this->shapeColor = $color;
@@ -46,9 +36,6 @@ class InlineGauge extends Column
         return $this;
     }
 
-    /**
-     * @param  string | Closure | null  $color
-     */
     public function sectionColor(string | Closure | null $color): static
     {
         $this->sectionColor = $color;
@@ -76,54 +63,35 @@ class InlineGauge extends Column
         return $this;
     }
 
-    /**
-     * @return string
-     */
     public function getSectionColor(): string
     {
         return $this->evaluate($this->sectionColor) ?? 'text-primary-600';
     }
 
-    /**
-     * @return string
-     */
     public function getShapeColor(): string
     {
         return $this->evaluate($this->shapeColor) ?? 'text-gray-300 dark:text-gray-700';
     }
 
-    /**
-     * @return string
-     */
     public function getSize(): string
     {
         return $this->evaluate($this->size)?->value ?? InlineGaugeSize::SM->value;
     }
 
-    /**
-     * @return bool
-     */
     public function isValueHidden(): bool
     {
         return $this->evaluate($this->isValueHidden) ?? false;
     }
 
-    /**
-     * @param int | Closure | null  $upperBound
-     */
     public function upperBound(int | Closure | null $upperBound): static
     {
         $this->upperBound = $upperBound;
+
         return $this;
     }
 
-
-    /**
-     * @return int | float
-     */
     public function getUpperBound(): int | float
     {
         return $this->evaluate($this->upperBound) ?? 100;
     }
-
 }
